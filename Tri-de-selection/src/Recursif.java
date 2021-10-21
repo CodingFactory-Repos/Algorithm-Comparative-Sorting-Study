8bca68f67d9845dc12ac69dd0a1a808e74ced78e
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2021-10-21 11:38:57 +0200

    📒 - Add Iteratif & Recursif

--- Tri-de-selection/src/Recursif.java
+++ Tri-de-selection/src/Recursif.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
-public class Main {
+public class Recursif {
+    public static int Opérations = 0;
+    public static int Comparaisons = 0;
+    public static int Affectations = 0;
 
     
-    public static int[] selectedSort(int[] tableau) {
-        int i;
+    public static int[] selectedSort(int[] tableau, int i) {
         int min;
         int v;
         int pos;
-        
-        
-        
-        for (i = 0; i < tableau.length - 1; i++) {
+  
+        if (i < tableau.length - 1) {
             pos = i;
             for (v = i + 1; v < tableau.length; v++) {
                 if (tableau[v] < tableau[pos]) {
@@ -21,17 +21,20 @@ public class Main {
             tableau[pos] = tableau[i];
             tableau[i] = min;
         }
+        if (i < tableau.length - 1) {
+            selectedSort(tableau, i+1);
+        }
         return tableau;
     }
     public static void main(String[] args) {
         // write your code here
         // Clear console
-        System.out.print("\033\033");
+        System.out.print("\033[H\033[2J");
 
         // Tableau de test
         int[] tableau = { -1,6,29,9,7,8,5,-3,4,2,1,30,-30};
 
-        System.out.println("Tableau de test : " + Arrays.toString(selectedSort(tableau)));
+        System.out.println("Tableau de test : " + Arrays.toString(selectedSort(tableau, 0)));
 
     }
 }
