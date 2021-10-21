6bf758bf98e57634310f35d3dfa84b2ddf400051
Lucas Goncalves <lucasgoncalves@free.fr>
2021-10-21 11:36:26 +0200

    Update Main.java

--- Tri-insertion/src/Main.java
+++ Tri-insertion/src/Main.java
@@ -1,32 +1,66 @@
-import java.util.Arrays;
+import java.util.Arrays; 
+import java.util.Random;
 public class Main {
+	public static float affectation = 0;
+	public static float echanges= 0;
+	public static float comparaison = 0;
      public static void main(String[] args) {
           System.out.print("\033\033");
   
-          int[] tableau = { 1, 3, 7, 2, 6, 4, 5, 8, 10, 9 };
+          int[] tableau = {  1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
   
           System.out.println("Tableau de test : " + Arrays.toString(tri(tableau)));
+          System.out.println("Nombre d'échanges : "+ echanges);
+          System.out.println("Nombre de comparaisons :" + comparaison);
+          System.out.println("Nombre d'affectations :" + affectation);
+         stat(10,20,5,10);
   
       }
      
     public static int[] tri(int[] tableau) 
     {  
-         int taille = tableau.length;   
+         int taille = tableau.length; 
+        
        
          for (int i = 1; i < taille; i++)
          { 
               int x = tableau[i];  
-              int j = i-1;  
+              int j = i-1; 
+    
+              affectation += 2;
+              comparaison++;
+              
            
               while(j >= 0 && tableau[j] > x)  
               {
-                   tableau[j+1] = tableau[j];  
+                   tableau[j+1] = tableau[j]; 
+                   echanges++;
+                   affectation+=1;
+                   comparaison+=2;
                    j--;  
-              }  
-              tableau [j+1] = x; 
+                   
+                   
+              } 
+              tableau [j+1] = x;
+              
        }  
-       return tableau;
+                return tableau;
     }
- 
+    
+    public static void stat(int min,int max, int step, int nbr) {
+        int i;
   
+        for ( i = 1 ; i <= nbr ; i++) {
+            int tableau[] =new Random().ints(min).toArray();
+            tri(tableau); 
+        }
+        if (min < max && i>nbr) {
+            System.out.println("Le nombre d'affectations pour " + min + " est de " + affectation/nbr);
+            stat((min+step),max,step,nbr);
+        } else {
+            System.out.println("Le nombre d'affectations pour " + min + " est de " + affectation/nbr);
+        }
+
+  
+}
 }
