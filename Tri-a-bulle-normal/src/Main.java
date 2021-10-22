f0d510f7b9fb9461238ab7adad37fd6d09c49a2c
Louis <35641311+Loule95450@users.noreply.github.com>
2021-10-22 10:31:31 +0200

    📊 - Update Stats

--- Tri-a-bulle-normal/src/Main.java
+++ Tri-a-bulle-normal/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-       public static float echanges = 0;
-       public static float comparaisons = 0;
-       public static float affectations = 0;
+        public static float echanges = 0;
+        public static float comparaisons = 0;
+        public static float affectations = 0;
 
         public static void main(String[] args) {
                 // write your code here
@@ -12,19 +12,15 @@ public class Main {
                 System.out.print("\033[H\033[2J");
 
                 // Tableau de test
-                // int[] tab = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+                // int[] tab = { 4, 3, 2, 1};
 
                 // System.out.println("Tableau de test : " + Arrays.toString(tri_bulleN(tab)));
-                
-                stat(10,20,5,10);
 
+                stat(10, 20, 5, 1);
 
         }
 
         public static int[] tri_bulleN(int[] tab) {
-                 echanges = 0;
-                 comparaisons = 0;
-                 affectations = 0;
                 int taille = tab.length;
                 int tmp = 0;
                 for (int i = 0; i < taille; i++) {
@@ -35,34 +31,40 @@ public class Main {
                                         tab[j - 1] = tab[j];
                                         tab[j] = tmp;
 
-                                        echanges++;     
+                                        echanges++;
                                         comparaisons++;
                                         affectations += 3;
                                 }
                                 comparaisons++;
+                                affectations++;
                         }
-                comparaisons++;
-                } 
-                  
-                
-                //System.out.println("Il y a eu " + comparaisons + " comparaisons, " + echanges + " échange et " + affectations + " affectations");
+
+                        affectations++;
+                        comparaisons++;
+                }
+
+                // System.out.println("Il y a eu " + comparaisons + " comparaisons, " + echanges + " échange et " + affectations + " affectations");
                 return tab;
         }
 
-public static void stat(int min,int max, int step, int nbr) {
-        int i;
-        comparaisons = 0;
-        echanges = 0;
-        affectations = 0;
-        for ( i = 1 ; i <= nbr ; i++) {
-            int Tab[] =new Random().ints(min).toArray();
-            tri_bulleN(Tab);
+        public static void stat(int min, int max, int step, int nbr) {
+                int i;
+                comparaisons = 0;
+                echanges = 0;
+                affectations = 0;
+                for (i = 1; i <= nbr; i++) {
+                        int Tab[] = new Random().ints(min).toArray();
+                        // int Tab[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                        // int Tab[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+                        tri_bulleN(Tab);
+                }
+                if (min < max && i > nbr) {
+                        System.out.println("Le nombre d'affectations pour " + min + " est de " + affectations / nbr);
+                        stat((min + step), max, step, nbr);
+                } else {
+                        System.out.println("Le nombre d'affectations pour " + min + " est de " + affectations / nbr);
+                        System.out.println("Le nombre de comparaisons est de " + comparaisons / nbr);
+                        System.out.println("Le nombre d'echanges est de " + echanges / nbr);
+                }
         }
-        if (min < max && i>nbr) {
-                System.out.println("Le nombre d'affectations pour " + min + " est de " + affectations/nbr);
-                stat((min+step),max,step,nbr);
-            } else {
-                System.out.println("Le nombre d'affectations pour " + min + " est de " + affectations/nbr);
-            }
-    }
 }
